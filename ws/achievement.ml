498e287b3fb8e11d1bf12c724fc3725ab4880470
db0company <db0company@gmail.com>
2012-11-14 02:01:26 +0100

    Achievement: Select the user in the achievement

--- ws/achievement.ml
+++ ws/achievement.ml
@@ -18,3 +18,18 @@ let table = <:table< achievement (
   default_privacy integer NOT NULL (* does macaque support custom enum? *)
 ) >>
 
+let select id =
+  let id = Int32.of_int id in
+  Db.select_first
+    (<:select<
+        { a = row_a ; u = row_u } |
+	 row_a in $table$ ; row_u in $User.table$ ;
+         row_a.id = $int32:id$ >>)
+
+let _ =
+  lwt a = select 1 in
+  Lwt.return
+    (match a with
+      | Some a -> print_endline a#!u#!login
+      | None   -> print_endline "none")
+
