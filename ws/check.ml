70fc3f186fe80db1416702cb3b2eb635f564f07e
db0company <db0company@gmail.com>
2012-11-18 23:40:50 +0100

    Coding style for validity checker module

---
+++ ws/check.ml
@@ -0,0 +1,22 @@
+(* ************************************************************************** *)
+(* Project: La Vie Est Un Jeu - API                                           *)
+(* Description: Tools to check data validity                                  *)
+(* Author: Tuxkowo                                                            *)
+(* Latest Version is on GitHub: https://github.com/LaVieEstUnJeu/API          *)
+(* ************************************************************************** *)
+
+(* Check if the given login is valid:                                         *)
+(* - contain only letters, numbers, underscores ans dash                      *)
+(* - start with a letter                                                      *)
+(* - lenght >= 2 and <= 64                                                    *)
+let login login =
+  Str.string_match (Str.regexp "^[a-zA-Z][a-zA-Z0-9_-]{1,63}$") login 0
+
+(* Check if the password is valid:                                            *)
+(* - lenght >= 8                                                              *)
+let password password =
+  (String.length password) >= 8
+
+(* Check if the e-mail is valid                                               *)
+let email email =
+  Str.string_match (Str.regexp "^[a-z0-9_.+-]+@([^.]+\\.)+[^.]+$") email 0
---
+++ ws/check.mli
@@ -0,0 +1,20 @@
+(* ************************************************************************** *)
+(* Project: La Vie Est Un Jeu - API                                           *)
+(* Description: Tools to check data validity                                  *)
+(* Author: Tuxkowo                                                            *)
+(* Latest Version is on GitHub: https://github.com/LaVieEstUnJeu/API          *)
+(* ************************************************************************** *)
+
+(* Check if the given login is valid:                                         *)
+(* - contain only letters, numbers, underscores ans dash                      *)
+(* - start with a letter                                                      *)
+(* - lenght >= 2 and <= 64                                                    *)
+val login    : string -> bool
+
+(* Check if the password is valid:                                            *)
+(* - lenght >= 8                                                              *)
+val password : string -> bool
+
+(* Check if the e-mail is valid                                               *)
+val email    : string -> bool
+
